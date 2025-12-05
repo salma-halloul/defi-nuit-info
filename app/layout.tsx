6bbe35059e984a4e4488512e9f773e9a5550de82
Selma Halloul <salmahalloul02@gmail.com>
2025-12-05 04:13:17 +0100

    add analytcs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Chatbot from "./components/Chatbot";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
       >
         {children}
         <Chatbot />
+        <Analytics/>
       </body>
     </html>
   );
